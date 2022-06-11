826386055d3660c50aa1521b2f821a6b89fa5b5f
fdelgadov <fdelgadov@unsa.edu.pe>
2022-06-10 20:05:32 -0500

    reverse: Se crean arreglos con malloc y se muestra dibujo incompleto
    - Se crean arreglos con malloc y ya no hay error "segmentation fault"

--- chess.c
+++ chess.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char** reverse(char** fig){
-  char** res;
+  char** res = (char**) malloc(sizeof(char*) * 58);
   char** pfig = fig;
   char* lineR;
   char* lineF;
   int i = 0;
   while(*res){
+    *res = (char*) malloc(sizeof(char) * 58);
     lineR = *res;
     lineF = *pfig;
     while(*lineR){
@@ -21,6 +23,5 @@ char** reverse(char** fig){
     pfig++;
   }
   printf("length: %d\n", i);
-  res = fig;
   return res;
 }
