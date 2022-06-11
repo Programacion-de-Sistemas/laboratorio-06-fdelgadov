07537caeb8e69c505cfe4ef39319db0fa9f8ff8c
fdelgadov <fdelgadov@unsa.edu.pe>
2022-06-10 19:22:24 -0500

    reverse: Intento falido al crear un arreglo caracter por caracter
    - Se muestra el mensaje "Segmentation fault" y termina el programa

--- chess.c
+++ chess.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
 char** reverse(char** fig){
-  char** res = fig;
+  char** res;
+  char** pfig = fig;
+  char* lineR;
+  char* lineF;
   int i = 0;
   while(*res){
-    char* line = *res;
-    while(*line){
-      printf("%c", *line);
-      line++;
+    lineR = *res;
+    lineF = *pfig;
+    while(*lineR){
+      *lineR = *lineF;
+      printf("%c", *lineR);
+      lineR++;
+      lineF++;
       i++;
     }
     printf("\n");
     res++;
+    pfig++;
   }
   printf("length: %d\n", i);
   res = fig;
