97fdd640c031c480081672c8f4db24d48345f1ea
fdelgadov <fdelgadov@unsa.edu.pe>
2022-06-11 12:35:33 -0500

    join: Intento de usar join con la misma figura

--- ej01.c
+++ ej01.c
@@ -5,7 +5,6 @@ void display(){
   char** blackSquare = reverse(whiteSquare);
   char** twoSquares = join(whiteSquare, blackSquare);
   freeMem(blackSquare);
-  char** row = repeatH(twoSquares, 4);
-  freeMem(twoSquares);
-  interpreter(row);
+  char** fourSquares = join(twoSquares, twoSquares);
+  interpreter(fourSquares);
 }
