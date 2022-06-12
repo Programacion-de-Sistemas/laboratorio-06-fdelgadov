ec1e24723da726a52b9db128c977aa3aa14f5e59
fdelgadov <fdelgadov@unsa.edu.pe>
2022-06-12 12:13:03 -0500

    ej04: Se crea la imagen correctamente

--- ej04.c
+++ ej04.c
@@ -2,6 +2,27 @@
 #include "figures.h"
 
 void display(){
-  char** test = superImpose(reverse(bishop), whiteSquare);
-  interpreter(test);
+  char** rk = join(rook, knight);
+  char** lateralA = join(rk, bishop);
+  freeMem(rk);
+  char** qk = join(queen, king);
+  char** bk = join(bishop, knight);
+  char** lateralB = join(bk, rook);
+  freeMem(bk);
+  char** aux = join(lateralA, qk);
+  freeMem(lateralA);
+  freeMem(qk);
+  char** backLine = join(aux, lateralB);
+  freeMem(lateralB);
+
+  char** blackSquare = reverse(whiteSquare);
+  char** twoSquares = join(blackSquare, whiteSquare);
+  freeMem(blackSquare);
+  char** row = repeatH(twoSquares, 4);
+
+  char** res = superImpose(backLine, row);
+  freeMem(backLine);
+  freeMem(row);
+
+  interpreter(res);
 }
